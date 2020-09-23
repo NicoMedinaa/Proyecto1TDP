3197c5a41234d5a90a232c53dd0fe452aaf5faf6
Nico <nimedina42@gmail.com>
2020-09-23 10:33:05 -0300

    Segundo commit para el conflicto

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,7 +2,7 @@
 public class Prueba {
 	
 	public static void main(String [] args) {
-		Punto a = new Punto(4,2); 
+		Punto a = new Punto(9,2); 
 		System.out.println("Punto: "+ a.valorX()+", "+a.valorY());
 	}
 }
